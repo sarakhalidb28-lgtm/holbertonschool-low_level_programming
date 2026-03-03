b735f7d673f16fc67dc3436d865a901a9cd404b3
Sara <Sarakhalid.b28@gmail.com.com>
2026-03-03 11:15:44 +0000

    Fix puts2 boundary issue

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,15 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	i = 0;
-	while (str[i] != '\0')
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
