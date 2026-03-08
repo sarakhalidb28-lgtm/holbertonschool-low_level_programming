1a6a15dd468401ab52459d6ca8e76e61fc78dca0
Sara20074 <Sarakhalid.b28@gmai.com>
2026-03-08 11:06:13 +0000

    Task 7 print chessboard

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
