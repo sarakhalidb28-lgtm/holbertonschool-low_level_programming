c0afd74cd484cb4f2689e267f1e45be4b172fad7
Sara20074 <Sarakhalid.b28@gmai.com>
2026-03-08 10:51:25 +0000

    Task 2 strchr

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 int _putchar(char c);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
