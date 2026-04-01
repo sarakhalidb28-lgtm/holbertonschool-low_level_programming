435a02d51e3b648e6c68a5863884b5f16f17aecf
Sara20074 <Sarakhalid.b28@gmail.com>
2026-04-01 09:49:03 +0000

    Add free_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
  * typedef struct dog dog_t - new name for struct dog
  */
 typedef struct dog dog_t;
-
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
