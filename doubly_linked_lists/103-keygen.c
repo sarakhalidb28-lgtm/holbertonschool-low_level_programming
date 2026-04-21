e37b73afcea21ea10018ac235414b1c33784b13b
Sara <sarakhalid.b28@gmail.com>
2026-04-21 13:41:12 +0300

    Update key generation logic in 103-keygen.c

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - generates a key for crackme5
+ * main - generates a valid key for crackme5
  * @argc: argument count
  * @argv: argument vector
  *
@@ -11,7 +11,8 @@
 int main(int argc, char *argv[])
 {
 	char *username;
-	int i, sum = 0;
+	unsigned long hash = 0;
+	int i;
 
 	if (argc != 2)
 		return (1);
@@ -19,9 +20,9 @@ int main(int argc, char *argv[])
 	username = argv[1];
 
 	for (i = 0; username[i]; i++)
-		sum += username[i];
+		hash = hash * 31 + username[i];
 
-	printf("%d\n", sum);
+	printf("%lu\n", hash);
 
 	return (0);
 }
