22e7eb71d8296ccab8f50162bc537f512b5c2401
Sara <sarakhalid.b28@gmail.com>
2026-04-22 12:39:16 +0300

    Add function prototype for append_text_to_file

--- file_io/main.h
+++ file_io/main.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
-
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 
