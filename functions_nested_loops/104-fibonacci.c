b738a5b34c7230094fa1d3884466e6472592e365
Sara <sarakhalid.b28@gmail.com>
2026-04-22 13:45:48 +0300

    Refactor Fibonacci calculation for clarity and efficiency

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,37 +7,33 @@
  */
 int main(void)
 {
-	unsigned long a1 = 1, a2 = 2, b1, b2;
-	unsigned long h1, h2;
+	unsigned long a1 = 1, a2 = 2;
+	unsigned long h1 = 0, h2 = 0;
+	unsigned long n1, n2;
 	int i;
 
 	printf("%lu, %lu", a1, a2);
 
 	for (i = 3; i <= 98; i++)
 	{
-		/* تقسيم الرقم إلى جزئين */
-		h1 = a1 / 1000000000;
-		h2 = a2 / 1000000000;
-		b1 = a1 % 1000000000;
-		b2 = a2 % 1000000000;
+		n1 = a1 + a2;
+		n2 = h1 + h2;
 
-		/* جمع الأجزاء */
-		h1 = h1 + h2;
-		b1 = b1 + b2;
-
-		if (b1 >= 1000000000)
+		if (n1 >= 1000000000)
 		{
-			h1 += 1;
-			b1 -= 1000000000;
+			n2 += 1;
+			n1 -= 1000000000;
 		}
 
-		if (h1 > 0)
-			printf(", %lu%09lu", h1, b1);
+		if (n2 > 0)
+			printf(", %lu%09lu", n2, n1);
 		else
-			printf(", %lu", b1);
+			printf(", %lu", n1);
 
 		a1 = a2;
-		a2 = h1 * 1000000000 + b1;
+		a2 = n1;
+		h1 = h2;
+		h2 = n2;
 	}
 
 	printf("\n");
