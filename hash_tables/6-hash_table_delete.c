bcb41d351fbca3b9a3c9eec60cf14bd1ee880d4d
Sara <sarakhalid.b28@gmail.com>
2026-04-26 12:41:45 +0300

    Add hash_table_delete function to free hash table
    Implement a function to delete a hash table and free its nodes.

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
